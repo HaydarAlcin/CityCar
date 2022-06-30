5dc5c5c76c2e80e3aa08925d9c9b521f89552cae
Haydar Alçin <hydralcin@gmail.com>
2022-06-30 17:09:07 +0300

    Visual Studio City Car Oyunu
    v1.0

---
+++ Ecodation_City_Car_Oyunu/Ecodation_City_Car_Oyunu/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace Ecodation_City_Car_Oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.car1 = new System.Windows.Forms.PictureBox();
+            this.car2 = new System.Windows.Forms.PictureBox();
+            this.coin1 = new System.Windows.Forms.PictureBox();
+            this.coin2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.scoreLbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Goldenrod;
+            this.label1.Location = new System.Drawing.Point(-2, -3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 711);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Goldenrod;
+            this.label2.Location = new System.Drawing.Point(602, -3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 711);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(320, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 140);
+            this.label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(320, 270);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 140);
+            this.label4.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(320, 530);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 140);
+            this.label5.TabIndex = 4;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Image = global::Ecodation_City_Car_Oyunu.Properties.Resources.car;
+            this.player.Location = new System.Drawing.Point(278, 512);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(98, 147);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 5;
+            this.player.TabStop = false;
+            // 
+            // car1
+            // 
+            this.car1.BackColor = System.Drawing.Color.Transparent;
+            this.car1.Image = global::Ecodation_City_Car_Oyunu.Properties.Resources.car1;
+            this.car1.Location = new System.Drawing.Point(100, 145);
+            this.car1.Name = "car1";
+            this.car1.Size = new System.Drawing.Size(94, 176);
+            this.car1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.car1.TabIndex = 6;
+            this.car1.TabStop = false;
+            // 
+            // car2
+            // 
+            this.car2.BackColor = System.Drawing.Color.Transparent;
+            this.car2.Image = global::Ecodation_City_Car_Oyunu.Properties.Resources.car5;
+            this.car2.Location = new System.Drawing.Point(442, 145);
+            this.car2.Name = "car2";
+            this.car2.Size = new System.Drawing.Size(103, 169);
+            this.car2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.car2.TabIndex = 7;
+            this.car2.TabStop = false;
+            // 
+            // coin1
+            // 
+            this.coin1.BackColor = System.Drawing.Color.Transparent;
+            this.coin1.Image = global::Ecodation_City_Car_Oyunu.Properties.Resources.coin;
+            this.coin1.Location = new System.Drawing.Point(124, 53);
+            this.coin1.Name = "coin1";
+            this.coin1.Size = new System.Drawing.Size(60, 60);
+            this.coin1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.coin1.TabIndex = 8;
+            this.coin1.TabStop = false;
+            // 
+            // coin2
+            // 
+            this.coin2.BackColor = System.Drawing.Color.Transparent;
+            this.coin2.Image = global::Ecodation_City_Car_Oyunu.Properties.Resources.coin;
+            this.coin2.Location = new System.Drawing.Point(464, 53);
+            this.coin2.Name = "coin2";
+            this.coin2.Size = new System.Drawing.Size(60, 60);
+            this.coin2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.coin2.TabIndex = 9;
+            this.coin2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 40;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // scoreLbl
+            // 
+            this.scoreLbl.BackColor = System.Drawing.Color.Transparent;
+            this.scoreLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.scoreLbl.ForeColor = System.Drawing.Color.White;
+            this.scoreLbl.Location = new System.Drawing.Point(35, -3);
+            this.scoreLbl.Name = "scoreLbl";
+            this.scoreLbl.Size = new System.Drawing.Size(126, 40);
+            this.scoreLbl.TabIndex = 10;
+            this.scoreLbl.Text = "Score: 0";
+            this.scoreLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Ecodation_City_Car_Oyunu.Properties.Resources.Yol;
+            this.ClientSize = new System.Drawing.Size(632, 703);
+            this.Controls.Add(this.scoreLbl);
+            this.Controls.Add(this.car2);
+            this.Controls.Add(this.car1);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.coin1);
+            this.Controls.Add(this.coin2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox car1;
+        private System.Windows.Forms.PictureBox car2;
+        private System.Windows.Forms.PictureBox coin1;
+        private System.Windows.Forms.PictureBox coin2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label scoreLbl;
+    }
+}
+
---
+++ Ecodation_City_Car_Oyunu/Ecodation_City_Car_Oyunu/Form1.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ecodation_City_Car_Oyunu
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random();
+
+        int score = 0;
+        int line1Y=10;
+        int line2Y=270;
+        int line3Y=530;
+        int car1X;
+        int car2X;
+        int car1Y;
+        int car2Y;
+        int coin1X;
+        int coin1Y;
+        int coin2X;
+        int coin2Y;
+
+        Image[] resimler = 
+           { 
+            Properties.Resources.car1,
+            Properties.Resources.car2,
+            Properties.Resources.car3,
+            Properties.Resources.car4,
+            Properties.Resources.car5,
+            Properties.Resources.car6,
+            Properties.Resources.car7,
+            Properties.Resources.car8,
+            Properties.Resources.car9,
+           };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            
+            
+
+            car1X = r.Next(40, 220);
+            car2X = r.Next(250, 400);
+
+            car1Y = r.Next(-500, -100);
+            car2Y = r.Next(-500, -100);
+
+            coin1X = r.Next(40, 220);
+            coin2X = r.Next(250, 400);
+
+            coin1Y = r.Next(-500, -100);
+            coin2Y = r.Next(-500, -100);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.Left)
+            {
+                player.Left -= 20;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                player.Left += 20;
+            }
+
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+
+            line1Y += 40;
+            line2Y += 40;
+            line3Y += 40;
+
+            
+
+            label3.Location = new Point(240, line1Y);
+            label4.Location = new Point(240, line2Y);
+            label5.Location = new Point(240, line3Y);
+
+            if (line1Y>700)
+            {
+                line1Y = -40;
+                label3.Location = new Point(240, line1Y);
+            }
+            if (line2Y > 700)
+            {
+                line2Y = -40;
+                label4.Location = new Point(240, line2Y);
+            }
+            if (line3Y > 700)
+            {
+                line3Y = -40;
+                label5.Location = new Point(240, line3Y);
+            }
+
+            
+
+            car1Y += 20;
+            car2Y += 20;
+            car1.Location = new Point(car1X, car1Y);
+            car2.Location = new Point(car2X, car2Y);
+
+            if (car1Y>700)
+            {
+                car1.Image = resimler[r.Next(0, 9)];
+                car1X = r.Next(40, 220);
+                car1Y = r.Next(-500, -100);
+                car1.Location = new Point(car1X, car1Y);
+            }
+            if (car2Y > 700)
+            {
+                car2.Image = resimler[r.Next(0, 9)];
+                car2X = r.Next(250, 400);
+                car2Y = r.Next(-500, -100);
+                car2.Location = new Point(car2X, car2Y);
+            }
+
+
+
+            coin1Y += 20;
+            coin2Y += 20;
+
+            coin1.Location = new Point(coin1X, coin1Y);
+            coin2.Location = new Point(coin2X, coin2Y);
+
+            if (coin1Y>700)
+            {
+                coin1X = r.Next(40, 220);
+                coin1Y = r.Next(-500, -100);
+                coin1.Location = new Point(coin1X, coin1Y);
+            }
+
+            if (coin2Y > 700)
+            {
+                coin2X = r.Next(250, 400);
+                coin2Y = r.Next(-500, -100);
+                coin2.Location = new Point(coin2X, coin2Y);
+            }
+
+            if (player.Bounds.IntersectsWith(label1.Bounds))
+            {
+                player.Left = +20;
+            }
+
+            if (player.Bounds.IntersectsWith(label1.Bounds))
+            {
+                player.Left = +20;
+            }
+
+            noLeft();
+            noRight();
+            
+            if (player.Bounds.IntersectsWith(car1.Bounds))
+            {
+                timer1.Enabled = false;
+                line_speed();
+                
+                timer2.Enabled = true;
+                
+
+                if (player.Left < car1.Left)
+                {
+                    player.Left -= 10;
+                }
+
+                else if (player.Left > car1.Left)
+                {
+                    player.Left += 10;
+                }
+                else
+                {
+                    player.Left += 10;
+                }
+                time_spin();
+                
+            }
+
+
+
+            if (player.Bounds.IntersectsWith(car2.Bounds))
+            {
+                timer1.Enabled = false;
+                line_speed();
+
+                timer2.Enabled = true;
+
+
+                if (player.Left < car2.Left)
+                {
+                    player.Left -= 10;
+                }
+
+                else if (player.Left > car2.Left)
+                {
+                    player.Left += 10;
+                }
+                else
+                {
+                    player.Left += 10;
+                }
+                time_spin();
+
+            }
+
+            if (player.Bounds.IntersectsWith(coin1.Bounds))
+            {
+                score++;
+                scoreLbl.Text = "Score: " + score;
+                coin1X = r.Next(40, 220);
+                coin1Y = r.Next(-500, -100);
+                coin1.Location = new Point(coin1X, coin1Y);
+            }
+
+            if (player.Bounds.IntersectsWith(coin2.Bounds))
+            {
+                score++;
+                scoreLbl.Text = "Score: " + score;
+                coin2X = r.Next(250, 400);
+                coin2Y = r.Next(-500, -100);
+                coin2.Location = new Point(coin2X, coin2Y);
+
+            }
+
+        }
+
+        private void noLeft()
+        {
+            if (player.Bounds.IntersectsWith(label1.Bounds))
+            {
+                player.Left += 20;
+            }
+        }
+        private void noRight()
+        {
+            if (player.Bounds.IntersectsWith(label2.Bounds))
+            {
+                player.Left -= 20;
+            }
+        }
+
+        
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            line_speed();
+            car1Y += 20;
+            car2Y += 20;
+            car1.Location = new Point(car1X, car1Y);
+            car2.Location = new Point(car2X, car2Y);
+
+            if (car1Y > 700)
+            {
+                car1.Image = resimler[r.Next(0, 9)];
+                car1X = r.Next(40, 220);
+                car1Y = r.Next(-500, -100);
+                car1.Location = new Point(car1X, car1Y);
+            }
+            if (car2Y > 700)
+            {
+                car2.Image = resimler[r.Next(0, 9)];
+                car2X = r.Next(250, 400);
+                car2Y = r.Next(-500, -100);
+                car2.Location = new Point(car2X, car2Y);
+            }
+            
+
+            label3.Location = new Point(240, line1Y);
+            label4.Location = new Point(240, line2Y);
+            label5.Location = new Point(240, line3Y);
+
+            if (line1Y > 700)
+            {
+                line1Y = -40;
+                label3.Location = new Point(240, line1Y);
+            }
+            if (line2Y > 700)
+            {
+                line2Y = -40;
+                label4.Location = new Point(240, line2Y);
+            }
+            if (line3Y > 700)
+            {
+                line3Y = -40;
+                label5.Location = new Point(240, line3Y);
+            }
+
+            
+        }
+
+        private void time_spin() 
+        {
+            timer1.Enabled = true;
+            timer2.Enabled = false;
+        }
+        private void line_speed()
+        {
+            line1Y +=0;
+            line2Y +=0;
+            line3Y +=0;
+        }
+    }
+
+
+
+}
